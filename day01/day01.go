8570cc49e93fdd7668ad50aa444c42be0a903916
David Seres <daseres@yettel.hu>
2024-12-01 10:51:20 +0100

    solve day1 part1

--- day01/day01.go
+++ day01/day01.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	dat, err := os.ReadFile("day01/input01.txt")
@@ -9,14 +14,43 @@ func main() {
 		panic(err)
 	}
 	input := string(dat[:])
-	fmt.Println("Day01 solution1:", solvePuzzle1(input))
+	nums1, nums2 := ParseInput(input)
+	fmt.Println("Day01 solution1:", solvePuzzle1(nums1, nums2))
 	fmt.Println("Day01 solution2:", solvePuzzle2(input))
 }
 
-func solvePuzzle1(input string) int64 {
-	return 0
+func solvePuzzle1(nums1 []int, nums2 []int) (diff int64) {
+	for i := range nums1 {
+		if nums1[i] < nums2[i] {
+			diff += int64(nums2[i] - nums1[i])
+		} else {
+			diff += int64(nums1[i] - nums2[i])
+		}
+	}
+	return
 }
 
 func solvePuzzle2(input string) int64 {
 	return 0
+}
+
+func ParseInput(input string) (array1 []int, array2 []int) {
+	for _, line := range strings.Split(input, "\n") {
+		if len(line) > 0 {
+			parts := strings.Fields(line)
+			i, err := strconv.Atoi(parts[0])
+			if err != nil {
+				panic(err)
+			}
+			j, err := strconv.Atoi(parts[1])
+			if err != nil {
+				panic(err)
+			}
+			array1 = append(array1, i)
+			array2 = append(array2, j)
+		}
+	}
+	slices.Sort(array1)
+	slices.Sort(array2)
+	return
 }
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -2,17 +2,23 @@ package main
 
 import "testing"
 
-var test_input = ""
+var testInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
 
 func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(test_input)
-	if result != 0 {
+	nums1, nums2 := ParseInput(testInput)
+	result := solvePuzzle1(nums1, nums2)
+	if result != 11 {
 		t.Errorf("Day01 puzzle1 solution on test input failed. Result: %v", result)
 	}
 }
 
 func TestSolution2(t *testing.T) {
-	result := solvePuzzle2(test_input)
+	result := solvePuzzle2(testInput)
 	if result != 0 {
 		t.Errorf("Day01 puzzle2 solution on test input failed. Result: %v", result)
 	}
