3b46fd5a2e313b0c2208eebaf3375b50910c5eda
David Seres <daseres@yettel.hu>
2024-12-03 10:34:20 +0100

    solve day03

--- day03/day03.go
+++ day03/day03.go
@@ -1,22 +1,63 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+//go:embed input03.txt
+var input string
 
 func main() {
-	dat, err := os.ReadFile("day03/input03.txt")
-	if err != nil {
-		panic(err)
-	}
-	input := string(dat[:])
 	fmt.Println("Day03 solution1:", solvePuzzle1(input))
 	fmt.Println("Day03 solution2:", solvePuzzle2(input))
 }
 
 func solvePuzzle1(input string) int64 {
-	return 0
+	re := regexp.MustCompile("mul\\((\\d+),(\\d+)\\)")
+	matches := re.FindAllStringSubmatch(input, -1)
+	sum := int64(0)
+	for _, m := range matches {
+		i, err := strconv.Atoi(m[1])
+		if err != nil {
+			panic(err)
+		}
+		j, err := strconv.Atoi(m[2])
+		if err != nil {
+			panic(err)
+		}
+		sum += int64(i * j)
+	}
+	return sum
 }
 
 func solvePuzzle2(input string) int64 {
-	return 0
+	re := regexp.MustCompile("do\\(\\)|don't\\(\\)|mul\\((\\d+),(\\d+)\\)")
+	matches := re.FindAllStringSubmatch(input, -1)
+	sum := int64(0)
+	do := true
+	for _, m := range matches {
+		if m[0] == "do()" {
+			do = true
+			continue
+		}
+		if m[0] == "don't()" {
+			do = false
+			continue
+		}
+		if do {
+			i, err := strconv.Atoi(m[1])
+			if err != nil {
+				panic(err)
+			}
+			j, err := strconv.Atoi(m[2])
+			if err != nil {
+				panic(err)
+			}
+			sum += int64(i * j)
+		}
+	}
+	return sum
 }
--- day03/day03_test.go
+++ day03/day03_test.go
@@ -1,19 +1,17 @@
 package main
 
 import "testing"
+import "github.com/stretchr/testify/assert"
 
-var test_input = ""
+var test_input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+var test_input2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 func TestSolution1(t *testing.T) {
 	result := solvePuzzle1(test_input)
-	if result != 0 {
-		t.Errorf("Day03 puzzle1 solution on test input failed. Result: %v", result)
-	}
+	assert.New(t).Equal(int64(161), result)
 }
 
 func TestSolution2(t *testing.T) {
-	result := solvePuzzle2(test_input)
-	if result != 0 {
-		t.Errorf("Day03 puzzle2 solution on test input failed. Result: %v", result)
-	}
+	result := solvePuzzle2(test_input2)
+	assert.New(t).Equal(int64(48), result)
 }
