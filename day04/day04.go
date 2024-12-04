d83ca99b98bb6b2a0bb343b02ac2b3b15ee38443
David Seres <daseres@yettel.hu>
2024-12-04 13:01:19 +0100

    solve day04 puzzle1

--- day04/day04.go
+++ day04/day04.go
@@ -1,46 +1,74 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"slices"
+)
 
 //go:embed input04.txt
-var input string
+var input []byte
 
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day04 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day04 solution2:", solvePuzzle2(nums))
+	text := parseInput(input)
+	printText(text)
+	_, count := solvePuzzle1(text)
+	fmt.Println("Day04 solution1:", count)
+	fmt.Println("Day04 solution2:", solvePuzzle2(text))
 }
 
-func solvePuzzle1(nums [][]int) int64 {
-	return 0
-}
-
-func solvePuzzle2(nums [][]int) int64 {
-	return 0
-}
-
-func parseInput(input string) (reports [][]int) {
-	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if len(line) > 0 {
-			report := parseLine(line)
-			reports = append(reports, report)
+func solvePuzzle1(text [][]byte) (representation [][]byte, counter int64) {
+	representation = slices.Repeat([][]byte{slices.Repeat([]byte{byte('.')}, len(text[0]))}, len(text))
+	xmas := []byte("XMAS")
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			if dx == 0 && dy == 0 {
+				continue
+			}
+			for i := range text {
+				for j := range text[i] {
+					found := true
+					for l := 0; l < len(xmas); l++ {
+						m, n := i+l*dx, j+l*dy
+						if m < 0 || len(text) <= m || n < 0 || len(text[i]) <= n {
+							found = false
+							break
+						}
+						if text[m][n] != xmas[l] {
+							found = false
+							break
+						}
+					}
+					if found {
+						for l := 0; l < len(xmas); l++ {
+							m, n := i+l*dx, j+l*dy
+							representation[m][n] = xmas[l]
+						}
+						counter++
+					}
+				}
+			}
 		}
 	}
 	return
 }
 
-func parseLine(line string) (report []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		report = append(report, val)
+func solvePuzzle2(text [][]byte) int64 {
+	return 0
+}
+
+func parseInput(input []byte) [][]byte {
+	text := bytes.Split(input, []byte("\n"))
+	// Remove last empty line if exists
+	if len(text) > 0 && len(text[len(text)-1]) == 0 {
+		text = text[:len(text)-1]
 	}
-	return
+	return text
+}
+
+func printText(text [][]byte) {
+	for _, line := range text {
+		fmt.Println(string(line))
+	}
 }
--- day04/day04_test.go
+++ day04/day04_test.go
@@ -1,18 +1,47 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"testing"
 
-var test_input = ""
+	"github.com/stretchr/testify/assert"
+)
+
+var testInput = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
+
+var testOutput = `....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX`
 
 func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(parseInput(test_input))
+	text := parseInput([]byte(testInput))
+	printText(text)
+	rep, count := solvePuzzle1(text)
+	fmt.Println()
+	printText(rep)
 	a := assert.New(t)
-	a.Equal(int64(0), result)
+	a.Equal(int64(18), count)
 }
 
 func TestSolution2(t *testing.T) {
-	result := solvePuzzle2(parseInput(test_input))
+	text := parseInput([]byte(testInput))
+	result := solvePuzzle2(text)
 	a := assert.New(t)
 	a.Equal(int64(0), result)
 }
