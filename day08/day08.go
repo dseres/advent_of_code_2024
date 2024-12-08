e583ab91881c72c0d41838418a055e72922de016
David Seres <daseres@yettel.hu>
2024-12-08 21:17:27 +0100

    solve day08

--- day08/day08.go
+++ day08/day08.go
@@ -1,46 +1,104 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"image"
+)
 
 //go:embed input08.txt
 var input string
 
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	f := createField(parseInput(input))
+	fmt.Println("Day07 solution1:", solvePuzzle1(f))
+	fmt.Println("Day07 solution2:", solvePuzzle2(f))
 }
 
-func solvePuzzle1(nums [][]int) int {
-	return 0
+func solvePuzzle1(f field) int {
+	return len(f.getAntinodes(f.computeAntinodes))
 }
 
-func solvePuzzle2(nums [][]int) int {
-	return 0
+func solvePuzzle2(f field) int {
+	return len(f.getAntinodes(f.computeAllAntinodes))
 }
 
-func parseInput(input string) (parsed [][]int) {
-	lines := strings.Split(input, "\n")
+func parseInput(input string) (parsed [][]byte) {
+	lines := bytes.Split([]byte(input), []byte{'\n'})
 	for _, line := range lines {
 		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
+			parsed = append(parsed, line)
 		}
 	}
 	return
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
+type point = image.Point
+type field struct {
+	input    [][]byte
+	antennas map[byte][]point
+}
+
+func createField(input [][]byte) (f field) {
+	f.input = input
+	f.antennas = make(map[byte][]point, 0)
+	for i, line := range input {
+		for j, n := range line {
+			if n != '.' {
+				if _, ok := f.antennas[n]; !ok {
+					f.antennas[n] = make([]point, 0)
+				}
+				f.antennas[n] = append(f.antennas[n], point{i, j})
+			}
 		}
-		nums = append(nums, val)
 	}
 	return
+}
+
+func (f *field) getAntinodes(compute func([]point) []point) map[point]bool {
+	antinodes := map[point]bool{}
+	for _, positions := range f.antennas {
+		for _, apos := range compute(positions) {
+			antinodes[apos] = true
+		}
+	}
+	return antinodes
+}
+
+func (f *field) computeAntinodes(positions []point) []point {
+	antinodes := []point{}
+	for i, p := range positions {
+		for _, q := range positions[i+1:] {
+			diff := p.Sub(q)
+			s, r := p.Add(diff), q.Sub(diff)
+			if f.valid(s) {
+				antinodes = append(antinodes, s)
+			}
+			if f.valid(r) {
+				antinodes = append(antinodes, r)
+			}
+		}
+	}
+	return antinodes
+}
+
+func (f *field) computeAllAntinodes(positions []point) []point {
+	antinodes := []point{}
+	for i, p := range positions {
+		for _, q := range positions[i+1:] {
+			diff := p.Sub(q)
+			for s := p; f.valid(s); s = s.Add(diff) {
+				antinodes = append(antinodes, s)
+			}
+			for s := p; f.valid(s); s = s.Sub(diff) {
+				antinodes = append(antinodes, s)
+			}
+		}
+	}
+	return antinodes
+}
+
+func (f *field) valid(p point) bool {
+	return 0 <= p.X && p.X < len(f.input) && 0 <= p.Y && p.Y < len(f.input[p.X])
 }
