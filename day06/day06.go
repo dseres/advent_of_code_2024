c99ee0f6d4a1798cd701fc536f362b552e7b5dc5
David Seres <daseres@yettel.hu>
2024-12-08 02:17:28 +0100

    day06 solved

--- day06/day06.go
+++ day06/day06.go
@@ -1,46 +1,118 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	_ "bufio"
+	"bytes"
+	_ "embed"
+	"fmt"
+	"image"
+	_ "os"
+	"slices"
+)
 
 //go:embed input06.txt
 var input string
 
+var directions []image.Point = []image.Point{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day06 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day06 solution2:", solvePuzzle2(nums))
+	fmt.Println("Day06 solution1:", solvePuzzle1(parseInput(input)))
+	fmt.Println("Day06 solution2:", solvePuzzle2(parseInput(input)))
 }
 
-func solvePuzzle1(nums [][]int) int64 {
-	return 0
+func solvePuzzle1(lab [][]byte, pos image.Point) int {
+	_, counter := walk(0, pos, lab)
+	return counter
 }
 
-func solvePuzzle2(nums [][]int) int64 {
-	return 0
+func solvePuzzle2(lab [][]byte, pos image.Point) int {
+	dir := 0
+	obstacles := map[image.Point]bool{}
+	start := pos
+	for {
+		valid := true
+		dir, valid = computeNextDirection(dir, pos, lab)
+		if !valid {
+			return len(obstacles)
+		}
+		nextPos := pos.Add(directions[dir])
+		// Checking loop on a cloned map starting with initial position and direction
+		newLab := clone(lab)
+		newLab[nextPos.X][nextPos.Y] = '#'
+		loop, _ := walk(0, start, newLab)
+		if loop {
+			obstacles[nextPos] = true
+		}
+		pos = nextPos
+	}
 }
 
-func parseInput(input string) (reports [][]int) {
-	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if len(line) > 0 {
-			report := parseLine(line)
-			reports = append(reports, report)
+func walk(dir int, pos image.Point, lab [][]byte) (bool, int) {
+	counter := 0
+	for {
+		if lab[pos.X][pos.Y] == '0'+byte(dir) {
+			// This is a loop
+			return true, counter
+		}
+		if lab[pos.X][pos.Y] == '.' {
+			counter++
+		}
+		lab[pos.X][pos.Y] = '0' + byte(dir)
+		valid := true
+		dir, valid = computeNextDirection(dir, pos, lab)
+		if !valid {
+			return false, counter
+		}
+		pos = pos.Add(directions[dir])
+	}
+}
+
+func computeNextDirection(dir int, pos image.Point, lab [][]byte) (new_dir int, valid bool) {
+	for i := 0; i < 4; i++ {
+		nextPos := pos.Add(directions[dir])
+		if !inRange(lab, nextPos) {
+			return dir, false
+		}
+		if lab[nextPos.X][nextPos.Y] != '#' {
+			return dir, true
+		}
+		dir = (dir + 1) % 4
+	}
+	panic("Position is invalid. It is between four obstacles.")
+}
+
+func inRange(m [][]byte, pos image.Point) bool {
+	return 0 <= pos.X && pos.X < len(m) && 0 <= pos.Y && pos.Y < len(m[pos.X])
+}
+
+func printMap(lab [][]byte) {
+	for _, line := range lab {
+		fmt.Println(string(line))
+	}
+}
+
+func clone(lab [][]byte) [][]byte {
+	newLab := [][]byte{}
+	for _, line := range lab {
+		newLab = append(newLab, slices.Clone(line))
+	}
+	return newLab
+}
+
+func parseInput(input string) (lab [][]byte, startPos image.Point) {
+	lab = bytes.Split([]byte(input), []byte{'\n'})
+	startPos = findStartPos(lab)
+	lab[startPos.X][startPos.Y] = '.'
+	return lab, startPos
+}
+
+func findStartPos(lab [][]byte) (position image.Point) {
+	for position.X = range lab {
+		for position.Y = range lab[position.X] {
+			if lab[position.X][position.Y] == '^' {
+				return
+			}
 		}
 	}
-	return
-}
-
-func parseLine(line string) (report []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		report = append(report, val)
-	}
-	return
+	panic("Cannot find start position.")
 }
--- day06/day06_test.go
+++ day06/day06_test.go
@@ -1,18 +1,82 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
 
-var test_input = ""
+	"github.com/stretchr/testify/assert"
+)
+
+var test_input = `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`
 
 func TestSolution1(t *testing.T) {
 	result := solvePuzzle1(parseInput(test_input))
 	a := assert.New(t)
-	a.Equal(int64(0), result)
+	a.Equal(41, result)
 }
 
 func TestSolution2(t *testing.T) {
 	result := solvePuzzle2(parseInput(test_input))
 	a := assert.New(t)
-	a.Equal(int64(0), result)
+	a.Equal(6, result)
+}
+
+func TestLoop0(t *testing.T) {
+	var input = `.#.
+.^#
+...
+#..
+.#.`
+	lab, pos := parseInput(input)
+	loop, count := walk(0, pos, lab)
+	a := assert.New(t)
+	a.True(loop)
+	a.Equal(3, count)
+	printMap(lab)
+}
+
+func TestLoop1(t *testing.T) {
+	var input = `.#.
+.^#
+#..
+.#.`
+	lab, pos := parseInput(input)
+	loop, count := walk(0, pos, lab)
+	a := assert.New(t)
+	a.True(loop)
+	a.Equal(2, count)
+	printMap(lab)
+}
+
+func TestLoop2(t *testing.T) {
+	a := assert.New(t)
+	var input = `.#.
+#^#
+.#.`
+	lab, pos := parseInput(input)
+	printMap(lab)
+	a.Panics(func() {
+		walk(0, pos, lab)
+	})
+}
+
+func TestLoop4(t *testing.T) {
+	var input = `.#..
+.^.#
+#...
+..#.`
+	lab, pos := parseInput(input)
+	loop, count := walk(0, pos, lab)
+	a := assert.New(t)
+	a.True(loop)
+	a.Equal(4, count)
+	printMap(lab)
 }
