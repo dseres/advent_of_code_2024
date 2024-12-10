8619cb575bf56a397c73638d9f5dd6efb63a9bb8
David Seres <daseres@yettel.hu>
2024-12-10 12:05:32 +0100

    solve day10 puzzle1

--- day10/day10.go
+++ day10/day10.go
@@ -1,28 +1,48 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	_ "embed"
+	"fmt"
+	"image"
+	"maps"
+	"strings"
+)
 
 //go:embed input10.txt
 var input string
 
+type tile struct {
+	height     uint8
+	visited    bool
+	trailheads map[point]bool
+}
+
+type point = image.Point
+
+var directions []point = []point{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	tiles := parseInput(input)
+	fmt.Println("Day07 solution1:", solvePuzzle1(tiles))
+	fmt.Println("Day07 solution2:", solvePuzzle2(tiles))
 }
 
-func solvePuzzle1(nums [][]int) int {
+func solvePuzzle1(tiles [][]tile) int {
+	starts := findStartPoints(tiles)
+	fmt.Println(starts)
+	sum := 0
+	for _, sp := range starts {
+		sum += len(search4routes(sp, tiles))
+	}
+	fmt.Println(tiles)
+	return sum
+}
+
+func solvePuzzle2(tiles [][]tile) int {
 	return 0
 }
 
-func solvePuzzle2(nums [][]int) int {
-	return 0
-}
-
-func parseInput(input string) (parsed [][]int) {
+func parseInput(input string) (parsed [][]tile) {
 	lines := strings.Split(input, "\n")
 	for _, line := range lines {
 		if len(line) > 0 {
@@ -33,14 +53,49 @@ func parseInput(input string) (parsed [][]int) {
 	return
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		nums = append(nums, val)
+func parseLine(line string) (tiles []tile) {
+	for _, c := range line {
+		height := uint8(c - '0')
+		tiles = append(tiles, tile{height: height, trailheads: map[point]bool{}})
 	}
 	return
+}
+
+func findStartPoints(tiles [][]tile) (starts []point) {
+	fmt.Println(tiles)
+	for x, line := range tiles {
+		for y, t := range line {
+			if t.height == 0 {
+				starts = append(starts, point{X: x, Y: y})
+			}
+		}
+	}
+	return
+}
+
+func search4routes(sp point, tiles [][]tile) map[point]bool {
+	if tiles[sp.X][sp.Y].height == 9 {
+		tiles[sp.X][sp.Y].visited = true
+		tiles[sp.X][sp.Y].trailheads[sp] = true
+		return tiles[sp.X][sp.Y].trailheads
+	}
+	trailheads := map[point]bool{}
+	for _, d := range directions {
+		np := sp.Add(d)
+		if valid(np, tiles) &&
+			tiles[sp.X][sp.Y].height+1 == tiles[np.X][np.Y].height {
+			if !tiles[np.X][np.Y].visited {
+				maps.Copy(trailheads, search4routes(np, tiles))
+			} else {
+				maps.Copy(trailheads, tiles[np.X][np.Y].trailheads)
+			}
+		}
+	}
+	tiles[sp.X][sp.Y].visited = true
+	tiles[sp.X][sp.Y].trailheads = trailheads
+	return trailheads
+}
+
+func valid(p point, tiles [][]tile) bool {
+	return 0 <= p.X && p.X < len(tiles) && 0 <= p.Y && p.Y < len(tiles[p.X])
 }
--- day10/day10_test.go
+++ day10/day10_test.go
@@ -3,12 +3,20 @@ package main
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-var testInput = ``
+var testInput = `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+`
 
 func TestSolution1(t *testing.T) {
 	result := solvePuzzle1(parseInput(testInput))
 	a := assert.New(t)
-	a.Equal(0, result)
+	a.Equal(36, result)
 }
 
 func TestSolution2(t *testing.T) {
