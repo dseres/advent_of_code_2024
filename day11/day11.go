a6e0186fcd279189dd56407b8760065fb1704ffa
David Seres <daseres@yettel.hu>
2024-12-11 10:03:30 +0100

    solve day11 puzzle2

--- day11/day11.go
+++ day11/day11.go
@@ -9,23 +9,24 @@ import (
 
 var input string = "64599 31 674832 2659361 1 0 8867 321"
 
+type elem struct {
+	num, round int
+}
+
+var cache map[elem]int = make(map[elem]int, 0)
+
 func main() {
 	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums, 25))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums, 25))
+	fmt.Println("Day07 solution1:", solvePuzzle(nums, 25))
+	fmt.Println("Day07 solution2:", solvePuzzle(nums, 75))
 }
 
-func solvePuzzle1(nums []int, round int) int {
-	fmt.Println(nums)
-	for i := 0; i < round; i++ {
-		nums = blink(nums)
-		fmt.Println(nums)
+func solvePuzzle(nums []int, round int) int {
+	sum := 0
+	for _, n := range nums {
+		sum += count(n, round)
 	}
-	return len(nums)
-}
-
-func solvePuzzle2(nums []int, round int) int {
-	return 0
+	return sum
 }
 
 func parseInput(input string) (nums []int) {
@@ -40,20 +41,28 @@ func parseInput(input string) (nums []int) {
 	return
 }
 
-func blink(nums []int) []int {
-	next := make([]int, 0, len(nums)*2)
-	for _, n := range nums {
-		digits := len(strconv.Itoa(n))
-		if n == 0 {
-			next = append(next, 1)
-		} else if digits&1 == 0 {
-			dividor := int(math.Pow10(digits / 2))
-			left := n % dividor
-			right := n / dividor
-			next = append(next, left, right)
-		} else {
-			next = append(next, n*2024)
-		}
+func count(num, round int) int {
+	if round == 0 {
+		return 1
 	}
-	return next
+	if c, ok := cache[elem{num, round}]; ok {
+		return c
+	}
+	count := blink(num, round)
+	cache[elem{num, round}] = count
+	return count
+}
+
+func blink(num, round int) int {
+	if num == 0 {
+		return count(1, round-1)
+	}
+	digits := len(strconv.Itoa(num))
+	if digits&1 == 0 {
+		dividor := int(math.Pow10(digits / 2))
+		left := num % dividor
+		right := num / dividor
+		return count(left, round-1) + count(right, round-1)
+	}
+	return count(num*2024, round-1)
 }
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -1,19 +1,16 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 var testInput = "125 17"
 
 func TestSolution1(t *testing.T) {
 	nums := parseInput(testInput)
 	a := assert.New(t)
-	a.Equal(22, solvePuzzle1(nums, 6))
-	a.Equal(55312, solvePuzzle1(nums, 25))
-}
-
-func TestSolution2(t *testing.T) {
-	result := solvePuzzle2(parseInput(testInput), 0)
-	a := assert.New(t)
-	a.Equal(0, result)
+	a.Equal(22, solvePuzzle(nums, 6))
+	a.Equal(55312, solvePuzzle(nums, 25))
 }
