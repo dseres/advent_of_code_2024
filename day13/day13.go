e8d44d6dd31de79b576849b6a3d9ec9cc1924e3d
David Seres <daseres@yettel.hu>
2024-12-13 09:42:40 +0100

    solve day13

--- day13/day13.go
+++ day13/day13.go
@@ -1,46 +1,116 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 //go:embed input13.txt
 var input string
 
+const (
+	A_TOKEN = 3
+	B_TOKEN = 1
+)
+
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	cms := parseInput(input)
+	fmt.Println("Day07 solution1:", solvePuzzle1(cms))
+	prepareForPuzzle2(cms)
+	fmt.Println("Day07 solution2:", solvePuzzle1(cms))
 }
 
-func solvePuzzle1(nums [][]int) int {
-	return 0
-}
-
-func solvePuzzle2(nums [][]int) int {
-	return 0
-}
-
-func parseInput(input string) (parsed [][]int) {
-	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
+func solvePuzzle1(cms []clawMachine) int {
+	sum := 0
+	for _, cm := range cms {
+		if minCost, ok := getMinCost2(cm); ok {
+			sum += minCost
 		}
+	}
+	return sum
+}
+
+func solvePuzzle2(cms []clawMachine) int {
+	sum := 0
+	for _, cm := range cms {
+		if minCost, ok := getMinCost2(cm); ok {
+			sum += minCost
+		}
+	}
+	return sum
+}
+
+type clawMachine struct {
+	ax, ay, bx, by, px, py int
+}
+
+func parseInput(input string) (parsed []clawMachine) {
+	blocks := strings.Split(input, "\n\n")
+	for _, block := range blocks {
+		cm := parseClawMachine(block)
+		parsed = append(parsed, cm)
 	}
 	return
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
+func parseClawMachine(block string) (cm clawMachine) {
+	lines := strings.Split(block, "\n")
+	rb := regexp.MustCompile(`^Button [AB]: X\+(\d+), Y\+(\d+)\s*$`)
+	rm := regexp.MustCompile(`^Prize: X=(\d+), Y=(\d+)\s*$`)
+	m1 := rb.FindAllStringSubmatch(lines[0], -1)
+	m2 := rb.FindAllStringSubmatch(lines[1], -1)
+	m3 := rm.FindAllStringSubmatch(lines[2], -1)
+	ax, _ := strconv.Atoi(m1[0][1])
+	ay, _ := strconv.Atoi(m1[0][2])
+	bx, _ := strconv.Atoi(m2[0][1])
+	by, _ := strconv.Atoi(m2[0][2])
+	px, _ := strconv.Atoi(m3[0][1])
+	py, _ := strconv.Atoi(m3[0][2])
+	return clawMachine{ax, ay, bx, by, px, py}
+}
+
+func getMinCost(cm clawMachine) (int, bool) {
+	validMoves := []int{}
+	for i := 0; i <= 100; i++ {
+		for j := 0; j <= 100; j++ {
+			if cm.ax*i+cm.bx*j == cm.px && cm.ay*i+cm.by*j == cm.py {
+				cost := i*A_TOKEN + j*B_TOKEN
+				validMoves = append(validMoves, cost)
+			}
 		}
-		nums = append(nums, val)
 	}
-	return
+	if len(validMoves) == 0 {
+		return 0, false
+	}
+	return slices.Min(validMoves), true
+}
+
+func prepareForPuzzle2(cms []clawMachine) {
+	for i := range cms {
+		cm := &cms[i]
+		cm.px += 10000000000000
+		cm.py += 10000000000000
+	}
+}
+
+func getMinCost2(cm clawMachine) (int, bool) {
+	j1 := cm.ax*cm.py - cm.ay*cm.px
+	j2 := cm.ax*cm.by - cm.ay*cm.bx
+	fmt.Println(cm, j1, j2, j1%j2)
+	if j1%j2 == 0 {
+		j := j1 / j2
+		i1 := (cm.px - j*cm.bx)
+		i2 := cm.ax
+		fmt.Println(i1, i2, i1%i2)
+		if i1%i2 == 0 {
+			i := i1 / i2
+			fmt.Println(i, j, i*A_TOKEN+j*B_TOKEN)
+			return i*A_TOKEN + j*B_TOKEN, true
+		}
+	}
+	return 0, false
 }
--- day13/day13_test.go
+++ day13/day13_test.go
@@ -1,18 +1,41 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
 
-var testInput = ``
+	"github.com/stretchr/testify/assert"
+)
 
-func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(parseInput(testInput))
+var testInput = `Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+`
+
+func TestSolution(t *testing.T) {
+	cms := parseInput(testInput)
+	result := solvePuzzle1(cms)
 	a := assert.New(t)
-	a.Equal(0, result)
-}
+	a.Equal(480, result)
 
-func TestSolution2(t *testing.T) {
-	result := solvePuzzle2(parseInput(testInput))
-	a := assert.New(t)
-	a.Equal(0, result)
+	prepareForPuzzle2(cms)
+	_, ok0 := getMinCost2(cms[0])
+	a.False(ok0)
+	_, ok1 := getMinCost2(cms[1])
+	a.True(ok1)
+	_, ok2 := getMinCost2(cms[2])
+	a.False(ok2)
+	_, ok3 := getMinCost2(cms[3])
+	a.True(ok3)
 }
