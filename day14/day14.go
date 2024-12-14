b0519ed1a41f3dd780f32fab06ac96edc6f3e185
David Seres <daseres@yettel.hu>
2024-12-14 09:36:33 +0100

    solve day14 puzzle1

--- day14/day14.go
+++ day14/day14.go
@@ -1,46 +1,128 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	_ "embed"
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 //go:embed input14.txt
 var input string
 
+type robot struct {
+	px, py, vx, vy int
+}
+
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	fmt.Println("Day07 solution1:", solvePuzzle1(parseInput(input)))
+	fmt.Println("Day07 solution2:", solvePuzzle2(parseInput(input)))
 }
 
-func solvePuzzle1(nums [][]int) int {
+func solvePuzzle1(robots []robot) int {
+	rect := getRectangle(robots)
+	fmt.Println(rect)
+	for i := range robots {
+		robots[i].px, robots[i].py = getPositionAfter(robots[i], 100, rect)
+	}
+	mx := (rect.vx - rect.px + 1) / 2
+	my := (rect.vy - rect.py + 1) / 2
+	a, b, c, d := 0, 0, 0, 0
+	for _, r := range robots {
+		if r.px < mx && r.py < my {
+			a++
+		}
+		if r.px > mx && r.py < my {
+			b++
+		}
+		if r.px < mx && r.py > my {
+			c++
+		}
+		if r.px > mx && r.py > my {
+			d++
+		}
+	}
+	fmt.Println(robots)
+	fmt.Println(a, b, c, d)
+	return a * b * c * d
+}
+
+func solvePuzzle2(robots []robot) int {
+	rect := getRectangle(robots)
+	fmt.Println(rect)
+	printRobots(rect, robots)
 	return 0
 }
 
-func solvePuzzle2(nums [][]int) int {
-	return 0
-}
-
-func parseInput(input string) (parsed [][]int) {
+func parseInput(input string) (robots []robot) {
 	lines := strings.Split(input, "\n")
 	for _, line := range lines {
 		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
+			robots = append(robots, parseLine(line))
 		}
 	}
 	return
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		nums = append(nums, val)
-	}
+func parseLine(line string) (r robot) {
+	strs := regexp.MustCompile("-?\\d+").FindAllString(line, -1)
+	r.px, _ = strconv.Atoi(strs[0])
+	r.py, _ = strconv.Atoi(strs[1])
+	r.vx, _ = strconv.Atoi(strs[2])
+	r.vy, _ = strconv.Atoi(strs[3])
 	return
+}
+
+func getRectangle(robots []robot) robot {
+	minx, miny := math.MaxInt, math.MaxInt
+	maxx, maxy := math.MinInt, math.MinInt
+	for _, r := range robots {
+		minx, miny = min(minx, r.px), min(miny, r.py)
+		maxx, maxy = max(maxx, r.px), max(maxy, r.py)
+	}
+	return robot{minx, miny, maxx, maxy}
+}
+
+func min[t ~int](a, b t) t {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max[t ~int](a, b t) t {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func getPositionAfter(r robot, round int, rect robot) (x, y int) {
+	return getCoordinateAfter(r.px, r.vx, rect.vx-rect.px+1, round),
+		getCoordinateAfter(r.py, r.vy, rect.vy-rect.py+1, round)
+}
+
+func getCoordinateAfter(from, speed, width, round int) int {
+	return (from + round*(speed+width)) % width
+}
+
+func printRobots(rect robot, robots []robot) {
+	for i := range rect.vx - rect.px + 1 {
+		for j := range rect.vy - rect.py + 1 {
+			count := 0
+			for _, r := range robots {
+				if r.px == rect.px+i && r.py == rect.py+j {
+					count++
+				}
+			}
+			if count == 0 {
+				fmt.Print(".")
+			} else {
+				fmt.Printf("%v", count)
+			}
+		}
+		fmt.Println()
+	}
 }
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -1,14 +1,45 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"testing"
 
-var testInput = ``
+	"github.com/stretchr/testify/assert"
+)
+
+var testInput = `p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+`
+
+func TestGetPosition(t *testing.T) {
+	a := assert.New(t)
+
+	rect := robot{0, 0, 10, 6}
+	r := robot{2, 4, 2, -3}
+
+	x, y := getPositionAfter(r, 4, rect)
+	a.Equal(10, x)
+	a.Equal(6, y)
+	x, y = getPositionAfter(r, 5, rect)
+	a.Equal(1, x)
+	a.Equal(4, y)
+}
 
 func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(parseInput(testInput))
 	a := assert.New(t)
-	a.Equal(0, result)
+	robots := parseInput(testInput)
+	fmt.Println(robots)
+	a.Equal(12, solvePuzzle1(robots))
 }
 
 func TestSolution2(t *testing.T) {
