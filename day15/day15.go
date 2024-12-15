85f289298da2931465cf175c01475512eaebfe9b
David Seres <daseres@yettel.hu>
2024-12-15 13:43:53 +0100

    solve day15 puzzle1

--- day15/day15.go
+++ day15/day15.go
@@ -1,46 +1,144 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"image"
+)
 
 //go:embed input15.txt
 var input string
 
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	wh := newWarehouse(input)
+	fmt.Println("Day07 solution1:", solvePuzzle1(wh))
+	fmt.Println("Day07 solution2:", solvePuzzle2(wh))
 }
 
-func solvePuzzle1(nums [][]int) int {
+func solvePuzzle1(wh warehouse) int {
+	fmt.Println(wh)
+	wh.execInstructions()
+	fmt.Println()
+	fmt.Println(wh)
+	return wh.boxSum()
+}
+
+func solvePuzzle2(wh warehouse) int {
 	return 0
 }
 
-func solvePuzzle2(nums [][]int) int {
-	return 0
+const (
+	emptyByte byte = '.'
+	wallByte  byte = '#'
+	boxByte   byte = 'O'
+	robotByte byte = '@'
+
+	upByte    byte = '^'
+	rightByte byte = '>'
+	downByte  byte = 'v'
+	leftByte  byte = '<'
+)
+
+type point = image.Point
+
+var (
+	upPoint    point = point{-1, 0}
+	rightPoint point = point{0, 1}
+	downPoint  point = point{1, 0}
+	leftPoint  point = point{0, -1}
+
+	directions   []point        = []point{upPoint, rightPoint, downPoint, leftPoint}
+	directionMap map[byte]point = map[byte]point{
+		upByte:    upPoint,
+		rightByte: rightPoint,
+		downByte:  downPoint,
+		leftByte:  leftPoint,
+	}
+)
+
+type warehouse struct {
+	representation [][]byte
+	robot          point
+	instructions   []byte
 }
 
-func parseInput(input string) (parsed [][]int) {
-	lines := strings.Split(input, "\n")
-	for _, line := range lines {
+func newWarehouse(input string) warehouse {
+	parts := bytes.Split([]byte(input), []byte("\n\n"))
+	var wh warehouse
+	for x, line := range bytes.Split(parts[0], []byte("\n")) {
 		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
+			for y, b := range line {
+				if b == robotByte {
+					wh.robot = point{x, y}
+				}
+			}
+			wh.representation = append(wh.representation, line)
 		}
 	}
-	return
+
+	wh.instructions = bytes.ReplaceAll(parts[1], []byte("\n"), []byte{})
+	return wh
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		nums = append(nums, val)
+func (wh warehouse) String() string {
+	str := ""
+	for _, line := range wh.representation {
+		str += string(line) + "\n"
 	}
-	return
+	return str
+}
+
+func (wh *warehouse) execInstructions() {
+	for _, i := range wh.instructions {
+		wh.execOn(i, wh.robot)
+	}
+}
+
+func (wh *warehouse) execOn(i byte, p point) bool {
+	d, ok := directionMap[i]
+	if !ok {
+		panic(fmt.Sprintf("Invalid instruction: %v", rune(i)))
+	}
+	next := p.Add(d)
+	fmt.Println(string(i), p, next, string(*wh.get(next)))
+	switch *wh.get(next) {
+	case wallByte:
+		return false
+	case boxByte, robotByte:
+		if wh.execOn(i, next) {
+			wh.move(p, next)
+			return true
+		}
+		return false
+	case emptyByte:
+		wh.move(p, next)
+		return true
+	default:
+		panic(fmt.Sprintf("Invalid point in warehouse representation[%v]: %v", next, string(*wh.get(next))))
+	}
+}
+
+func (wh *warehouse) get(p point) *byte {
+	return &wh.representation[p.X][p.Y]
+}
+
+func (wh *warehouse) move(from, to point) {
+	*wh.get(to) = *wh.get(from)
+	*wh.get(from) = emptyByte
+	if from == wh.robot {
+		wh.robot = to
+	}
+}
+
+func (wh *warehouse) boxSum() int {
+	sum := 0
+	for x := range wh.representation {
+		for y := range wh.representation[x] {
+			if *wh.get(point{x, y}) == boxByte {
+				sum += 100*x + y
+			}
+		}
+	}
+	return sum
 }
--- day15/day15_test.go
+++ day15/day15_test.go
@@ -1,18 +1,54 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
 
-var testInput = ``
+	"github.com/stretchr/testify/assert"
+)
+
+var testInput1 = `##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`
+
+var testInput2 = `########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<`
 
 func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(parseInput(testInput))
 	a := assert.New(t)
-	a.Equal(0, result)
+	wh1 := newWarehouse(testInput1)
+	wh2 := newWarehouse(testInput2)
+	a.Equal(2048, solvePuzzle1(wh2))
+	a.Equal(10092, solvePuzzle1(wh1))
 }
 
 func TestSolution2(t *testing.T) {
-	result := solvePuzzle2(parseInput(testInput))
+	result := solvePuzzle2(newWarehouse(testInput2))
 	a := assert.New(t)
 	a.Equal(0, result)
 }
