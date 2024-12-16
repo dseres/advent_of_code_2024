f4ef0adb324996e8752aaa0388b7381d86f3ba7f
David Seres <daseres@yettel.hu>
2024-12-16 10:22:41 +0100

    solve day16 puzzle 1
    It is solved with Dijkstra algorythm

--- day16/day16.go
+++ day16/day16.go
@@ -1,46 +1,104 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"image"
+	"math"
+)
 
 //go:embed input16.txt
 var input string
 
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	maze := parseInput(input)
+	fmt.Println("Day07 solution1:", solvePuzzle1(maze))
+	fmt.Println("Day07 solution2:", solvePuzzle2(maze))
 }
 
-func solvePuzzle1(nums [][]int) int {
+func parseInput(input string) [][]byte {
+	return bytes.Split([]byte(input), []byte("\n"))
+}
+
+const (
+	emptyByte byte = '.'
+	wallByte  byte = '#'
+	startByte byte = 'S'
+	endByte   byte = 'E'
+
+	toTheRight int = 1
+	toTheLeft  int = 3
+)
+
+type point = image.Point
+
+var (
+	upPoint    point = point{-1, 0}
+	rightPoint point = point{0, 1}
+	downPoint  point = point{1, 0}
+	leftPoint  point = point{0, -1}
+
+	dirPoints = []point{upPoint, rightPoint, downPoint, leftPoint}
+)
+
+type node struct {
+	position            point
+	direction, distance int
+}
+
+func solvePuzzle1(maze [][]byte) int {
+	// TODO: write a Dijkstra algorythm
+	//  * find reachable points
+	n := node{startPoint(maze), toTheRight, 0}
+	// fmt.Println(n)
+	reachables := map[point]node{}
+	visited := map[point]int{n.position: n.distance}
+	for {
+		addReachableNodes(maze, visited, n, &reachables)
+		n.distance = math.MaxInt
+		for _, r := range reachables {
+			if r.distance < n.distance {
+				n = r
+			}
+		}
+		// fmt.Println(n, reachables, visited)
+		visited[n.position] = n.distance
+		delete(reachables, n.position)
+		if maze[n.position.X][n.position.Y] == endByte {
+			return n.distance
+		}
+	}
+}
+
+func solvePuzzle2(maze [][]byte) int {
 	return 0
 }
 
-func solvePuzzle2(nums [][]int) int {
-	return 0
-}
-
-func parseInput(input string) (parsed [][]int) {
-	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
+func startPoint(maze [][]byte) point {
+	for x, l := range maze {
+		for y, b := range l {
+			if b == startByte {
+				return point{x, y}
+			}
 		}
 	}
-	return
+	panic("Maze doesn't have a start point.")
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
+func addReachableNodes(maze [][]byte, visited map[point]int, n node, reachables *map[point]node) {
+	addReachable(maze, visited, n, 0, 1, reachables)
+	addReachable(maze, visited, n, toTheRight, 1001, reachables)
+	addReachable(maze, visited, n, toTheLeft, 1001, reachables)
+}
+
+func addReachable(maze [][]byte, visited map[point]int, n node, dir, dist int, reachables *map[point]node) {
+	nextDir := (n.direction + dir) % 4
+	nextPos := n.position.Add(dirPoints[nextDir])
+	value := maze[nextPos.X][nextPos.Y]
+	if value != wallByte {
+		if _, ok := visited[nextPos]; !ok {
+			(*reachables)[nextPos] = node{nextPos, nextDir, n.distance + dist}
 		}
-		nums = append(nums, val)
 	}
-	return
 }
--- day16/day16_test.go
+++ day16/day16_test.go
@@ -3,16 +3,48 @@ package main
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-var testInput = ``
+var testInput1 = `###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############`
+
+var testInput2 = `#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################`
 
 func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(parseInput(testInput))
 	a := assert.New(t)
-	a.Equal(0, result)
+	a.Equal(7036, solvePuzzle1(parseInput(testInput1)))
+	a.Equal(11048, solvePuzzle1(parseInput(testInput2)))
 }
 
 func TestSolution2(t *testing.T) {
-	result := solvePuzzle2(parseInput(testInput))
+	result := solvePuzzle2(parseInput(testInput1))
 	a := assert.New(t)
 	a.Equal(0, result)
 }
