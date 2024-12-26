4e6bfd4638b9d119de7e419ac975d3e837218587
David Seres <daseres@yettel.hu>
2024-12-26 21:06:36 +0100

    add a converter for day21 solution

--- day21/day21.go
+++ day21/day21.go
@@ -1,46 +1,74 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	_ "embed"
+	"fmt"
+	"slices"
+	"strings"
+)
 
 //go:embed input21.txt
 var input string
 
+var numPad []byte = []byte("789456123X0A")
+var keyPad []byte = []byte("X^A<v>")
+
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	codes := parseInput(input)
+	fmt.Println(codes)
+	fmt.Println("Day07 solution1:", solvePuzzle1(codes))
+	fmt.Println("Day07 solution2:", solvePuzzle2(codes))
 }
 
-func solvePuzzle1(nums [][]int) int {
+func solvePuzzle1(codes []string) int {
 	return 0
 }
 
-func solvePuzzle2(nums [][]int) int {
+func solvePuzzle2(codes []string) int {
 	return 0
 }
 
-func parseInput(input string) (parsed [][]int) {
-	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
-		}
-	}
-	return
+func parseInput(input string) (parsed []string) {
+	return strings.Split(input, "\n")
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		nums = append(nums, val)
+func toNums(input []byte, level int) []byte {
+	var pad []byte = keyPad
+	if level == 0 {
+		pad = numPad
 	}
-	return
+	pos := slices.Index(pad, 'A')
+	output := []byte{}
+	for _, d := range input {
+		switch d {
+		case '^':
+			pos -= 3
+		case '<':
+			pos--
+		case '>':
+			pos++
+		case 'v':
+			pos += 3
+		case 'A':
+			if level == 0 {
+				output = append(output, pad[pos])
+			} else {
+				output = append(output, pad[pos])
+			}
+		default:
+			panic(fmt.Sprintf("bad instruction %v", d))
+		}
+	}
+	if level > 0 {
+		return toNums(output, level-1)
+	}
+	return output
 }
+
+// func routesOnNumPad() [][]byte {
+// 	for i, line := range numPad {
+// 		for j, v := range line {
+
+// 		}
+// 	}
+// }
--- day21/day21_test.go
+++ day21/day21_test.go
@@ -3,12 +3,41 @@ package main
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-var testInput = ``
+var testInput = `029A
+980A
+179A
+456A
+379A`
+
+/*
+029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
+980A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A
+179A: <v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
+456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
+379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
+*/
+
+func TestToNum(t *testing.T) {
+	a := assert.New(t)
+
+	a.Equal([]byte("029A"), toNums([]byte("<A^A>^^AvvvA"), 0))
+	a.Equal([]byte("029A"), toNums([]byte("<A^A^>^AvvvA"), 0))
+	a.Equal([]byte("029A"), toNums([]byte("<A^A^^>AvvvA"), 0))
+
+	a.Equal([]byte("029A"), toNums([]byte("v<<A>>^A<A>AvA<^AA>A<vAAA>^A"), 1))
+
+	a.Equal([]byte("029A"), toNums([]byte("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"), 2))
+	a.Equal([]byte("980A"), toNums([]byte("<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A"), 2))
+	a.Equal([]byte("179A"), toNums([]byte("<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"), 2))
+	a.Equal([]byte("456A"), toNums([]byte("<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A"), 2))
+	a.Equal([]byte("379A"), toNums([]byte("<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"), 2))
+
+}
 
 func TestSolution1(t *testing.T) {
 	result := solvePuzzle1(parseInput(testInput))
 	a := assert.New(t)
-	a.Equal(0, result)
+	a.Equal(0 /*126384*/, result)
 }
 
 func TestSolution2(t *testing.T) {
