ab979ce1a4fc3fa3325aa07fb871208cc137d310
David Seres <daseres@yettel.hu>
2025-01-02 20:24:43 +0100

    solve day22 puzzle1

--- day22/day22.go
+++ day22/day22.go
@@ -14,33 +14,38 @@ func main() {
 	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
 }
 
-func solvePuzzle1(nums [][]int) int {
+func solvePuzzle1(nums []int) int {
+	sum := 0
+	for i := range nums {
+		for range 2000 {
+			nums[i] = nextSecret(nums[i])
+		}
+		sum += nums[i]
+	}
+	return sum
+}
+
+func solvePuzzle2(nums []int) int {
 	return 0
 }
 
-func solvePuzzle2(nums [][]int) int {
-	return 0
-}
-
-func parseInput(input string) (parsed [][]int) {
-	lines := strings.Split(input, "\n")
+func parseInput(input string) (parsed []int) {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
 	for _, line := range lines {
 		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
+			num, err := strconv.Atoi(line)
+			if err != nil {
+				panic(err)
+			}
+			parsed = append(parsed, num)
 		}
 	}
 	return
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		nums = append(nums, val)
-	}
-	return
+func nextSecret(num int) int {
+	num = (num ^ (num * 64)) % 16777216
+	num = (num ^ int(float64(num)/32.0)) % 16777216
+	num = (num ^ (num * 2048)) % 16777216
+	return num
 }
--- day22/day22_test.go
+++ day22/day22_test.go
@@ -3,12 +3,35 @@ package main
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-var testInput = ``
+var testInput = `1
+10
+100
+2024`
+
+var testResults = []int{8685429, 4700978, 15273692, 8667524}
+
+func TestNextSecret(t *testing.T) {
+	a := assert.New(t)
+	num := 123
+	var secrets = []int{15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254}
+	for _, s := range secrets {
+		num = nextSecret(num)
+		a.Equal(num, s)
+	}
+
+	nums := parseInput(testInput)
+	for i, n := range nums {
+		for range 2000 {
+			n = nextSecret(n)
+		}
+		a.Equal(testResults[i], n)
+	}
+}
 
 func TestSolution1(t *testing.T) {
 	result := solvePuzzle1(parseInput(testInput))
 	a := assert.New(t)
-	a.Equal(0, result)
+	a.Equal(37327623, result)
 }
 
 func TestSolution2(t *testing.T) {
