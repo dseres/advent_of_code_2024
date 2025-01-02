8e93c68fa5530faa83f2ac2d33b2260d4b815cbc
David Seres <daseres@yettel.hu>
2025-01-02 18:11:21 +0100

    change the search of routes in keyboards much simpler

--- day21/day21.go
+++ day21/day21.go
@@ -4,7 +4,6 @@ import (
 	_ "embed"
 	"fmt"
 	"math"
-	"slices"
 	"strconv"
 	"strings"
 )
@@ -12,10 +11,10 @@ import (
 //go:embed input21.txt
 var input string
 
-var numPad = "789456123X0A"
-var dirPad = "X^A<v>"
-var numRoutes map[byte]map[byte][]string = getRoutes(numPad)
-var dirRoutes map[byte]map[byte][]string = getRoutes(dirPad)
+var numPad string = "789\n456\n123\nX0A"
+var dirPad string = "X^A\n<v>"
+var numRoutes map[rune]map[rune][]string = getRoutes(numPad)
+var dirRoutes map[rune]map[rune][]string = getRoutes(dirPad)
 
 type key struct {
 	level int
@@ -43,181 +42,59 @@ func parseInput(input string) (parsed []string) {
 	return strings.Split(strings.TrimSpace(input), "\n")
 }
 
-func toNums(input string, level int) string {
-	pad := padOf(level)
-	pos := strings.Index(pad, "A")
-	output := strings.Builder{}
-	for _, d := range input {
-		switch d {
-		case '^':
-			pos -= 3
-		case '<':
-			pos--
-		case '>':
-			pos++
-		case 'v':
-			pos += 3
-		case 'A':
-			output.WriteByte(pad[pos])
-		default:
-			panic(fmt.Sprintf("bad instruction %v", d))
+func getRoutes(pad string) (routes map[rune]map[rune][]string) {
+	routes = make(map[rune]map[rune][]string)
+	lines := strings.Split(pad, "\n")
+	// Iterating over characters as `from`
+	for y0, l0 := range lines {
+		for x0, from := range l0 {
+			if from == 'X' {
+				continue
+			}
+			routes[from] = make(map[rune][]string)
+			// Iterating over characters as `to`
+			for y1, l1 := range lines {
+				for x1, to := range l1 {
+					if to == 'X' {
+						continue
+					}
+					routes[from][to] = getRoutesFor(lines, x0, y0, x1, y1)
+				}
+			}
 		}
 	}
-	if level > 0 {
-		return toNums(output.String(), level-1)
+	return
+}
+
+func getRoutesFor(lines []string, x0, y0, x1, y1 int) []string {
+	dx := x1 - x0
+	xpart := ">>"[:abs(dx)]
+	if dx < 0 {
+		xpart = "<<"[:abs(dx)]
 	}
-	return output.String()
-}
-
-func padOf(level int) string {
-	pad := dirPad
-	if level == 0 {
-		pad = numPad
+	dy := y1 - y0
+	ypart := "vvv"[:abs(dy)]
+	if dy < 0 {
+		ypart = "^^^"[:abs(dy)]
 	}
-	return pad
-}
-
-type node struct {
-	from, to    byte
-	routes      [][]int // Routes are stored as sequence of index offsets. E.g.: [-3 -3 1 1]
-	index, dist int
-	prevs       []int
-}
-
-func getRoutes(pad string) map[byte]map[byte][]string {
-	rs := make(map[byte]map[byte][]string)
-	for i := range pad {
-		if pad[i] == 'X' {
-			continue
+	rs := make([]string, 0, 2)
+	if lines[y1][x0] != 'X' {
+		rs = append(rs, ypart+xpart+"A")
+	}
+	if lines[y0][x1] != 'X' {
+		r := xpart + ypart + "A"
+		if len(rs) == 0 || r != rs[0] {
+			rs = append(rs, xpart+ypart+"A")
 		}
-		rs[pad[i]] = searchRoutesFrom(pad, i)
 	}
 	return rs
 }
 
-func searchRoutesFrom(pad string, startInd int) map[byte][]string {
-	visited := make(map[byte]node)
-	start := pad[startInd]
-	visited[pad[startInd]] = node{from: start, to: start, index: startInd, routes: [][]int{{}}}
-	reachables := make(map[byte]node)
-	findReachables(pad, visited, reachables, visited[start])
-	for len(reachables) > 0 {
-		n := getNextReachable(reachables)
-		n = computeRoutesOfNode(pad, visited, n)
-		visited[n.to] = n
-		delete(reachables, n.to)
-		findReachables(pad, visited, reachables, n)
+func abs(a int) int {
+	if a < 0 {
+		return -a
 	}
-	return convertVisiteds(visited)
-}
-
-func findReachables(pad string, visited, reachables map[byte]node, n node) {
-	dirs := []int{-3, 3, -1, 1}
-	for _, dir := range dirs {
-		nextInd := n.index + dir
-		// Is next position is valid?
-		if dir == -1 && n.index%3 == 0 || dir == 1 && n.index%3 == 2 {
-			continue
-		}
-		if nextInd < 0 || len(pad) <= nextInd || pad[nextInd] == 'X' {
-			continue
-		}
-		to := pad[nextInd]
-		if _, ok := visited[to]; ok {
-			continue
-		}
-		// Check next node in reachables
-		switch r, ok := reachables[to]; {
-		case ok && n.dist+1 < r.dist: // A better route found to node
-			r.dist = n.dist + 1
-			r.prevs = []int{dir}
-			reachables[to] = r
-		case ok && r.dist == n.dist+1: // A new route found to node
-			r.prevs = append(r.prevs, dir)
-			reachables[to] = r
-		case !ok: // Add new node to reachables
-			r = node{from: n.from, to: to, index: nextInd, dist: n.dist + 1, prevs: []int{dir}}
-			reachables[to] = r
-		}
-	}
-}
-
-func getNextReachable(reachables map[byte]node) node {
-	n, min := node{}, math.MaxInt
-	for _, rn := range reachables {
-		if rn.dist < min {
-			n = rn
-			min = rn.dist
-		}
-	}
-	return n
-}
-
-func computeRoutesOfNode(pad string, visited map[byte]node, n node) node {
-	for _, dir := range n.prevs {
-		prevNode := visited[pad[n.index-dir]]
-		for _, prevRoute := range prevNode.routes {
-			route := slices.Clone(prevRoute)
-			route = append(route, dir)
-			n.routes = append(n.routes, route)
-		}
-	}
-	return n
-}
-
-func convertVisiteds(visited map[byte]node) map[byte][]string {
-	// Convert directions to string
-	routes := make(map[byte][]string)
-	for to, v := range visited {
-		strs := routesAsString(v.routes)
-		// filter only the best routes by rank
-		maxRank := math.MinInt
-		for _, s := range strs {
-			r := rank(s)
-			switch {
-			case maxRank < r:
-				routes[to] = []string{s}
-				maxRank = r
-			case r == maxRank:
-				routes[to] = append(routes[to], s)
-			}
-		}
-	}
-	return routes
-}
-
-func routesAsString(directions [][]int) []string {
-	strs := []string{}
-	for _, route := range directions {
-		builder := strings.Builder{}
-		for _, dir := range route {
-			switch dir {
-			case -3:
-				builder.WriteRune('^')
-			case 3:
-				builder.WriteRune('v')
-			case -1:
-				builder.WriteRune('<')
-			case 1:
-				builder.WriteRune('>')
-			}
-		}
-		builder.WriteRune('A')
-		strs = append(strs, builder.String())
-	}
-	return strs
-}
-
-func rank(code string) (rank int) {
-	if len(code) < 2 {
-		return 0
-	}
-	for i := range code[:len(code)-1] {
-		if code[i] == code[i+1] {
-			rank++
-		}
-	}
-	return
+	return a
 }
 
 func numOf(code string) int {
@@ -241,9 +118,8 @@ func codeLength(code string, level, maxLevel int) int {
 		return v
 	}
 	sum := 0
-	for i := range code[:len(code)-1] {
-		from := code[i]
-		to := code[i+1]
+	for i, from := range code[:len(code)-1] {
+		to := rune(code[i+1])
 		routes := routes[from][to]
 		min := math.MaxInt
 		for _, r := range routes {
--- day21/day21_test.go
+++ day21/day21_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,31 +19,6 @@ var testInput = `029A
 456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
 379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
 */
-
-func TestToNum(t *testing.T) {
-	a := assert.New(t)
-
-	a.Equal("029A", toNums("<A^A>^^AvvvA", 0))
-	a.Equal("029A", toNums("<A^A^>^AvvvA", 0))
-	a.Equal("029A", toNums("<A^A^^>AvvvA", 0))
-
-	a.Equal("029A", toNums("v<<A>>^A<A>AvA<^AA>A<vAAA>^A", 1))
-
-	a.Equal("029A", toNums("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A", 2))
-	a.Equal("980A", toNums("<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A", 2))
-	a.Equal("179A", toNums("<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A", 2))
-	a.Equal("456A", toNums("<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A", 2))
-	a.Equal("379A", toNums("<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A", 2))
-
-	a.Equal("029A", toNums("v<A<AA>^>AvA^<Av>A^Av<<A>^>AvA^Av<<A>^>AAv<A>A^A<A>Av<A<A>^>AAA<Av>A^A", 2))
-}
-
-func TestRoutes(t *testing.T) {
-	a := assert.New(t)
-	routesFromA := searchRoutesFrom(numPad, strings.Index(numPad, "A"))
-	a.Equal(2, len(routesFromA['5']))
-	a.Equal(1, len(routesFromA['7']))
-}
 
 func TestConvert(t *testing.T) {
 	a := assert.New(t)
