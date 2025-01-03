434f30933f1e17737ac71a3223ae40420d848d8d
David Seres <daseres@yettel.hu>
2025-01-03 17:39:05 +0100

    add a graph implementation to day23

--- day23/day23.go
+++ day23/day23.go
@@ -3,44 +3,50 @@ package main
 import "fmt"
 import _ "embed"
 import "strings"
-import "strconv"
 
 //go:embed input23.txt
 var input string
 
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	g := parseInput(input)
+	fmt.Println("Day07 solution1:", solvePuzzle1(g))
+	fmt.Println("Day07 solution2:", solvePuzzle2(g))
 }
 
-func solvePuzzle1(nums [][]int) int {
+func solvePuzzle1(g graph) int {
 	return 0
 }
 
-func solvePuzzle2(nums [][]int) int {
+func solvePuzzle2(g graph) int {
 	return 0
 }
 
-func parseInput(input string) (parsed [][]int) {
-	lines := strings.Split(input, "\n")
+type graph = map[int16][]int16
+
+func parseInput(input string) (g graph) {
+	g = make(graph)
+	lines := strings.Split(strings.TrimSpace(input), "\n")
 	for _, line := range lines {
-		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
-		}
+		a, b := parseLine(line)
+		insert(g, a, b)
+		insert(g, b, a)
 	}
 	return
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		nums = append(nums, val)
+func parseLine(line string) (a, b int16) {
+	s := strings.Split(line, "-")
+	return str2Int16(s[0]), str2Int16(s[1])
+}
+
+func str2Int16(s string) int16 {
+	return int16(s[0]-'a')*int16('z'-'a'+1) + int16(s[1]-'a')
+}
+
+func insert(g graph, from, to int16) {
+	if _, ok := g[from]; ok {
+		g[from] = append(g[from], to)
+		return
 	}
-	return
+	g[from] = []int16{to}
 }
--- day23/day23_test.go
+++ day23/day23_test.go
@@ -1,14 +1,78 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"slices"
+	"testing"
 
-var testInput = ``
+	"github.com/stretchr/testify/assert"
+)
+
+var testInput = `kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn
+`
+
+var interconnected = `aq,cg,yn
+aq,vc,wq
+co,de,ka
+co,de,ta
+co,ka,ta
+de,ka,ta
+kh,qp,ub
+qp,td,wh
+tb,vc,wq
+tc,td,wh
+td,wh,yn
+ub,vc,wq`
+
+func TestGraph(t *testing.T) {
+	a := assert.New(t)
+	a.Equal(int16(16), str2Int16("aq"))
+	a.Equal(int16(58), str2Int16("cg"))
+	g := parseInput(testInput)
+	a.Equal(4, len(g[16]))
+	for _, n := range g[16] {
+		a.True(slices.Contains(g[n], 16))
+	}
+}
 
 func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(parseInput(testInput))
+	g := parseInput(testInput)
+	fmt.Println('z' - 'a' + 1)
+	fmt.Println(g)
+	result := solvePuzzle1(g)
 	a := assert.New(t)
-	a.Equal(0, result)
+	a.Equal(0 /*7*/, result)
 }
 
 func TestSolution2(t *testing.T) {
