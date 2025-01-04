ad162b6007a29392cd3d77776fe7109ef565361b
David Seres <daseres@yettel.hu>
2025-01-04 20:09:49 +0100

    solve day24 part1

--- day24/day24.go
+++ day24/day24.go
@@ -1,46 +1,130 @@
 package main
 
-import "fmt"
-import _ "embed"
-import "strings"
-import "strconv"
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 //go:embed input24.txt
 var input string
 
+type machine struct {
+	startValues map[string]int
+	values      map[string]int
+	gates       map[string]gate
+}
+
+type gate struct {
+	left, right, dest string
+	operand           opFun
+}
+
+type opFun = func(a, b int) int
+
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	m := new(input)
+	fmt.Println("Day07 solution1:", solvePuzzle1(m))
+	fmt.Println("Day07 solution2:", solvePuzzle2(m))
 }
 
-func solvePuzzle1(nums [][]int) int {
-	return 0
+func new(input string) machine {
+	parts := strings.Split(strings.TrimSpace(input), "\n\n")
+	m := machine{}
+	m.values = parseValues(parts[0])
+	m.gates = parseGates(parts[1])
+	return m
 }
 
-func solvePuzzle2(nums [][]int) int {
-	return 0
-}
-
-func parseInput(input string) (parsed [][]int) {
-	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
-		}
-	}
-	return
-}
-
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
+func parseValues(input string) map[string]int {
+	values := map[string]int{}
+	for _, line := range strings.Split(input, "\n") {
+		parts := strings.Split(line, ": ")
+		v, err := strconv.Atoi(parts[1])
 		if err != nil {
 			panic(err)
 		}
-		nums = append(nums, val)
+		values[parts[0]] = v
 	}
-	return
+	return values
+}
+
+func parseGates(input string) map[string]gate {
+	gates := map[string]gate{}
+	re := regexp.MustCompile(`^(\w+) (\w+) (\w+) -> (\w+)$`)
+	for _, line := range strings.Split(input, "\n") {
+		sm := re.FindStringSubmatch(line)
+		g := gate{left: sm[1], operand: parseOp(sm[2]), right: sm[3], dest: sm[4]}
+		gates[sm[4]] = g
+	}
+	return gates
+}
+
+func parseOp(input string) opFun {
+	var op opFun
+	switch input {
+	case "AND":
+		op = func(a, b int) int {
+			fmt.Printf("%v AND %v -> %v\n", a, b, a&b)
+			return a & b
+		}
+	case "OR":
+		op = func(a, b int) int {
+			fmt.Printf("%v OR %v -> %v\n", a, b, a|b)
+			return a | b
+		}
+	case "XOR":
+		op = func(a, b int) int {
+			fmt.Printf("%v XOR %v -> %v\n", a, b, a^b)
+			return a ^ b
+		}
+	}
+	return op
+}
+
+func solvePuzzle1(m machine) int {
+	n := 0
+	for k := range m.gates {
+		if strings.HasPrefix(k, "z") {
+			m.compute(k)
+			n = n | m.intVal(k)
+		}
+	}
+	for k, v := range m.values {
+		fmt.Printf("%v = %v\n", k, v)
+	}
+	return n
+}
+
+func (m *machine) compute(wire string) {
+	g, ok := m.gates[wire]
+	fmt.Println(g)
+	if !ok {
+		panic(fmt.Sprintf("there is no operand for %v", wire))
+	}
+	if _, ok := m.values[g.left]; !ok {
+		m.compute(g.left)
+	}
+	if _, ok := m.values[g.right]; !ok {
+		m.compute(g.right)
+	}
+	value := g.operand(m.values[g.left], m.values[g.right])
+	m.values[wire] = value
+}
+
+func (m machine) intVal(wire string) int {
+	if !strings.HasPrefix(wire, "z") {
+		panic(fmt.Sprintf("intVal function can be called with z wire instead of %v", wire))
+	}
+	bit, err := strconv.Atoi(wire[1:])
+	if err != nil {
+		panic(err)
+	}
+	return m.values[wire] << bit
+}
+
+func solvePuzzle2(m machine) int {
+	return 0
 }
--- day24/day24_test.go
+++ day24/day24_test.go
@@ -1,18 +1,108 @@
 package main
 
-import "testing"
-import "github.com/stretchr/testify/assert"
+import (
+	"fmt"
+	"testing"
 
-var testInput = ``
+	"github.com/stretchr/testify/assert"
+)
+
+var testInput = `x00: 1
+x01: 0
+x02: 1
+x03: 1
+x04: 0
+y00: 1
+y01: 1
+y02: 1
+y03: 1
+y04: 1
+
+ntg XOR fgs -> mjb
+y02 OR x01 -> tnw
+kwq OR kpj -> z05
+x00 OR x03 -> fst
+tgd XOR rvg -> z01
+vdt OR tnw -> bfw
+bfw AND frj -> z10
+ffh OR nrd -> bqk
+y00 AND y03 -> djm
+y03 OR y00 -> psh
+bqk OR frj -> z08
+tnw OR fst -> frj
+gnj AND tgd -> z11
+bfw XOR mjb -> z00
+x03 OR x00 -> vdt
+gnj AND wpb -> z02
+x04 AND y00 -> kjc
+djm OR pbm -> qhw
+nrd AND vdt -> hwm
+kjc AND fst -> rvg
+y04 OR y02 -> fgs
+y01 AND x02 -> pbm
+ntg OR kjc -> kwq
+psh XOR fgs -> tgd
+qhw XOR tgd -> z09
+pbm OR djm -> kpj
+x03 XOR y03 -> ffh
+x00 XOR y04 -> ntg
+bfw OR bqk -> z06
+nrd XOR fgs -> wpb
+frj XOR qhw -> z04
+bqk OR frj -> z07
+y03 OR x01 -> nrd
+hwm AND bqk -> z03
+tgd XOR rvg -> z12
+tnw OR pbm -> gnj`
+
+var testResult = `bfw: 1
+bqk: 1
+djm: 1
+ffh: 0
+fgs: 1
+frj: 1
+fst: 1
+gnj: 1
+hwm: 1
+kjc: 0
+kpj: 1
+kwq: 0
+mjb: 1
+nrd: 1
+ntg: 0
+pbm: 1
+psh: 1
+qhw: 1
+rvg: 0
+tgd: 0
+tnw: 1
+vdt: 1
+wpb: 0
+z00: 0
+z01: 0
+z02: 0
+z03: 1
+z04: 0
+z05: 1
+z06: 1
+z07: 1
+z08: 1
+z09: 1
+z10: 1
+z11: 0
+z12: 0`
 
 func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(parseInput(testInput))
-	a := assert.New(t)
-	a.Equal(0, result)
-}
+	m := new(testInput)
+	fmt.Println(m)
 
-func TestSolution2(t *testing.T) {
-	result := solvePuzzle2(parseInput(testInput))
 	a := assert.New(t)
-	a.Equal(0, result)
+	a.Equal(2024, solvePuzzle1(m))
+
+	r := parseValues(testResult)
+	for k := range r {
+		a.Equal(r[k], m.values[k], "value of %v is bad", k)
+	}
+
+	a.Equal(0, solvePuzzle2(m))
 }
