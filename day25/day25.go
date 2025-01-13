e9a7a3c62dfb71fb8f786962cb90e65481680f50
David Seres <daseres@yettel.hu>
2025-01-13 16:23:35 +0100

    solve day25 puzzle 1

--- day25/day25.go
+++ day25/day25.go
@@ -3,44 +3,79 @@ package main
 import "fmt"
 import _ "embed"
 import "strings"
-import "strconv"
 
 //go:embed input25.txt
 var input string
 
 func main() {
-	nums := parseInput(input)
-	fmt.Println("Day07 solution1:", solvePuzzle1(nums))
-	fmt.Println("Day07 solution2:", solvePuzzle2(nums))
+	locks, keys := parseInput(input)
+	fmt.Println("Day07 solution1:", solvePuzzle1(locks, keys))
+	fmt.Println("Day07 solution2:", solvePuzzle2(locks, keys))
 }
 
-func solvePuzzle1(nums [][]int) int {
-	return 0
+func parseInput(input string) (locks [][]int, keys [][]int) {
+	blocks := strings.Split(strings.TrimSpace(input), "\n\n")
+	for _, block := range blocks {
+		parsed, isLock := parseBlock(block)
+		if isLock {
+			locks = append(locks, parsed)
+			continue
+		}
+		keys = append(keys, parsed)
+	}
+	return
 }
 
-func solvePuzzle2(nums [][]int) int {
-	return 0
-}
-
-func parseInput(input string) (parsed [][]int) {
+func parseBlock(input string) (parsed []int, isLock bool) {
 	lines := strings.Split(input, "\n")
-	for _, line := range lines {
-		if len(line) > 0 {
-			report := parseLine(line)
-			parsed = append(parsed, report)
+	isLock = checkBlock(lines)
+	parsed = make([]int, len(lines[0]))
+	for _, line := range lines[1 : len(lines)-1] {
+		for j, r := range line {
+			if r == '#' {
+				parsed[j]++
+			}
 		}
 	}
 	return
 }
 
-func parseLine(line string) (nums []int) {
-	fields := strings.Fields(line)
-	for _, field := range fields {
-		val, err := strconv.Atoi(field)
-		if err != nil {
-			panic(err)
-		}
-		nums = append(nums, val)
+func checkBlock(lines []string) bool {
+	isLock := false
+	if lines[0] == "#####" {
+		isLock = true
+	} else if lines[0] == "....." {
+		isLock = false
+	} else {
+		panic(fmt.Sprintf("bad block, it is not lock or key: %v", lines))
 	}
-	return
+	return isLock
+}
+
+func solvePuzzle1(locks [][]int, keys [][]int) int {
+	// fmt.Println(len(locks), locks, len(keys), keys)
+	count := 0
+	for _, lock := range locks {
+		for _, key := range keys {
+			// fmt.Println(lock, key, match(lock, key))
+			if match(lock, key) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func match(lock, key []int) bool {
+	for i, l := range lock {
+		k := key[i]
+		if 5 < l+k {
+			return false
+		}
+	}
+	return true
+}
+
+func solvePuzzle2(locks [][]int, keys [][]int) int {
+	return 0
 }
--- day25/day25_test.go
+++ day25/day25_test.go
@@ -3,12 +3,53 @@ package main
 import "testing"
 import "github.com/stretchr/testify/assert"
 
-var testInput = ``
+var testInput = `#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####`
 
 func TestSolution1(t *testing.T) {
-	result := solvePuzzle1(parseInput(testInput))
 	a := assert.New(t)
-	a.Equal(0, result)
+	locks, keys := parseInput(testInput)
+	a.Equal([][]int{[]int{0, 5, 3, 4, 3}, []int{1, 2, 0, 5, 3}}, locks)
+	a.Equal([][]int{[]int{5, 0, 2, 1, 3}, []int{4, 3, 4, 0, 2}, []int{3, 0, 2, 0, 1}}, keys)
+	result := solvePuzzle1(locks, keys)
+	a.Equal(3, result)
 }
 
 func TestSolution2(t *testing.T) {
